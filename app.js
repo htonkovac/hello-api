084ced3ae1af44a2eed280336328170ecadc4832
Hrvoje <htonkovac@gmail.com>
2019-08-08 11:27:11 +0200

    Removing dead code

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
+
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var cookieSession = require('cookie-session');
+
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var passport = require('passport');
@@ -15,9 +13,7 @@ var redisClient = redis.createClient({ host: 'localhost', port: 6379});
 var redisStore = require('connect-redis')(session);
 var app = express();
 
-app.use(bodyParser.json());
-app.use(cookieParser('random-key'));
-//app.use(cookieSession({secret: "random-key"}));
+
 var sessionMiddleware = session({
     name: "custom_session_cookie_name",
     store: new redisStore({client: redisClient}),
@@ -39,12 +35,9 @@ function isLoggedIn(req, res, next) {
     return res.status(401).send({status: 401, message: "You must be logged in."})
 }
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
 
 
 app.use(function (req, res, next) {
